91a4a7e7fa5e672f668713482eefb94dc7e2adf8
pitronum <pitronum@gmail.com>
2025-04-27 19:16:53 +0300

    added validation tags

--- types.go
+++ types.go
@@ -8,18 +8,18 @@ type Parameters struct {
 
 // meta is the general info about a message when it's not just a plain interaction.
 type Meta struct {
-	ActionId string `json:"action_id"`
+	ActionId string `json:"action_id" validate:"required"`
 }
 
 // message structurs in each interaction signal
 type Message struct {
-	From       string     `json:"from"`
-	Type       string     `json:"type"`
-	Body       string     `json:"body"`
-	Meta       Meta       `json:"meta"`
-	Parameters Parameters `json:"parameters"`
+	From       string      `json:"from" validate:"required"`
+	Type       string      `json:"type" validate:"required"`
+	Body       *string     `json:"body,omitempty"`
+	Meta       *Meta       `json:"meta,omitempty"`
+	Parameters *Parameters `json:"parameters,omitempty"`
 }
 
 type InteractionSchema struct {
-	Data []Message `json:"data"`
+	Data []Message `json:"data" validate:"required,dive"`
 }
